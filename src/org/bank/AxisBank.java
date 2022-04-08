43e88c52382f0fb04b23b6cc18d2407daa42c800
prabhakaran231997 <prabhakarancivil23@gmail.com>
2022-04-08 11:48:44 +0530

    done

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,10 @@
+package org.bank;
+
+public class AxisBank {
+	public void deposit() {
+		System.out.println("master");
+	}
+	public static void main(String[] args) {
+		System.out.println("mster");
+	}
+}
